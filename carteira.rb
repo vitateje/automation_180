be872f98ee0e2305bf8f6ece28312ae688d1ac08
vitateje <vitateje@gmail.com>
2021-04-08 14:38:09 -0300

    operações

---
+++ carteira.rb
@@ -0,0 +1,6 @@
+nome = "Vitor"
+idade = 32
+
+if (idade >= 18)
+    puts "Voce Pode tirar carteira de motorista"
+end
--- metodos.rb
+++ metodos.rb
@@ -2,8 +2,22 @@
 
 # padrão ruby para variáveis e métodos: snake_case
 
-def imprime_nome(nome)
-    puts "@vitateje", nome
+# def imprime_nome(nome)
+#     puts "@vitateje", nome
+# end
+
+# imprime_nome("teste")
+
+def pega_soma(valor1, valor2)
+    resultado = valor1 + valor2
+    return resultado
 end
 
-imprime_nome("teste")
+# o ruby sempre retorna a ultima instrução de codigo
+# def pega_soma1(valor1, valor2)
+#     resultado = valor1 + valor2
+#     resultado
+# end
+
+total = pega_soma(1,5)
+puts total
