ed592e06faf5e33d57ae5a1152c04e51b36c1417
vitateje <vitateje@gmail.com>
2021-04-08 16:49:06 -0300

    gets chomp

--- carteira.rb
+++ carteira.rb
@@ -1,10 +1,27 @@
-nome = "Vitor"
-idade = 6
+# nome = "Vitor"
+# idade = 6
+
+# if (idade >= 18)
+#     puts nome + " Get your Car Brow"
+# elsif (idade >= 7)
+#     puts nome + " Get de Bike Guy"
+# else
+#     puts "Nothing to Declare"
+# end
+
+# gets & chomp
+
+puts "Digite Seu Nome"
+nome = gets.chomp
+puts "Informe a sua Idade"
+idade = gets.chomp.to_i
+
+puts idade.class
 
 if (idade >= 18)
     puts nome + " Get your Car Brow"
-elsif (idade < 7)
-    puts nome + " Vai de Motoquinha"
+elsif (idade >= 7)
+    puts nome + " Get de Bike Guy"
 else
-    puts "Nothing to Declare"
+    puts "Nothing to Declare ," + nome
 end
