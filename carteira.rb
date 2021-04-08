8c3d521241d4a37aa223f1e1e53dbf5e2ef01b49
vitateje <vitateje@gmail.com>
2021-04-08 15:26:30 -0300

    carteira

--- carteira.rb
+++ carteira.rb
@@ -1,6 +1,10 @@
 nome = "Vitor"
-idade = 32
+idade = 6
 
 if (idade >= 18)
-    puts "Voce Pode tirar carteira de motorista"
+    puts nome + " Get your Car Brow"
+elsif (idade < 7)
+    puts nome + " Vai de Motoquinha"
+else
+    puts "Nothing to Declare"
 end
