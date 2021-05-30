f75a91cc119bfdbe0b90dd469a8a099b687fc25a
vitateje <vitateje@gmail.com>
2021-05-30 08:15:26 -0300

    timeouts implicitos - ok

--- rocklov/features/step_definitions/cadastro_steps.rb
+++ rocklov/features/step_definitions/cadastro_steps.rb
@@ -21,5 +21,5 @@ Quando('submeto o seguinte formulário de cadastro:') do |table|
 
     click_button "Cadastrar"
     
-    sleep 1
+    # sleep 3
 end
--- rocklov/features/support/env.rb
+++ rocklov/features/support/env.rb
@@ -7,5 +7,5 @@ require "mongo"
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
     config.app_host = "http://127.0.0.1:3000"
-    Capybara.default_max_wait_time = 5
+    Capybara.default_max_wait_time = 10
 end
