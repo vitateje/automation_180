c81c1f609c759f16f3d8d0b8b384ec8b28735e41
vitateje <vitateje@gmail.com>
2021-05-31 22:47:05 -0300

    allure framework - ok

--- rocklov/features/support/env.rb
+++ rocklov/features/support/env.rb
@@ -1,11 +1,16 @@
+require "allure-cucumber"
 require "capybara"
 require "capybara/cucumber"
 require "faker"
 require "mongo"
 
-
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
-    config.app_host = "http://127.0.0.1:3000"
-    Capybara.default_max_wait_time = 10
+  config.default_driver = :selenium_chrome
+  config.app_host = "http://127.0.0.1:3000"
+  Capybara.default_max_wait_time = 10
+end
+
+AllureCucumber.configure do |config|
+  config.results_directory = "/logs"
+  config.clean_results_directory = true
 end
