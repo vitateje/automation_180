fbc4eef7ed131e852c3c198f5b43f0440133d608
vitateje <vitateje@gmail.com>
2021-06-05 14:28:15 -0300

    cross browser - ok

--- rocklov/features/support/env.rb
+++ rocklov/features/support/env.rb
@@ -6,8 +6,21 @@ require "mongo"
 
 CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["CONFIG"]}"))
 
+case ENV["BROWSER"]
+when "firefox"
+  @driver = :selenium
+when "chrome"
+  @driver = :selenium_chrome
+when "fire_headless"
+  @driver = :selenium_headless
+when "chrome_headless"
+  @driver = :selenium_chrome_headless
+else
+  raise "Navegador Incorreto, variavel @driver está vazia :("
+end
+
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
+  config.default_driver = @driver
   config.app_host = CONFIG["url"]
   Capybara.default_max_wait_time = 10
 end
