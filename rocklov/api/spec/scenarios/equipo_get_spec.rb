0be2cba77649a25907d7825dd24628067c699f0b
vitateje <vitateje@gmail.com>
2022-11-18 16:10:45 -0300

    Equipo Not Found + refactor - ok

--- rocklov/api/spec/scenarios/equipo_get_spec.rb
+++ rocklov/api/spec/scenarios/equipo_get_spec.rb
@@ -41,5 +41,17 @@ describe "GET /equipos/{equipo_id}" do
 
     end
 
+    context "equipo nao existe" do
 
+        before(:all) do
+
+            @result = Equipos.new.find_by_id(MongoDB.new.get_mongo_id, @user_id)
+
+        end
+
+        it "deve retornar 404" do
+            expect(@result.code).to eql 404
+        end
+
+    end
 end
