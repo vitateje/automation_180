756f0b6718cd9974ef0961f8e772e1a737fd2e94
vitateje <vitateje@gmail.com>
2022-11-29 19:10:23 -0300

    test

--- rocklov/web/features/support/env.rb
+++ rocklov/web/features/support/env.rb
@@ -14,6 +14,20 @@ when "chrome"
 when "fire_headless"
   @driver = :selenium_headless
 when "chrome_headless"
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    version = Capybara::Selenium::Driver.load_selenium
+    options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
+    browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+      opts.add_argument('--headless')
+      opts.add_argument('--disable-gpu')
+      opts.add_argument('--disable-site-isolation-trials')
+      opts.add_argument('--no-sandbox')
+      opts.add_argument('--disabledev-shm-usage')
+    end
+  
+    Capybara::Selenium::Driver.new(app, **{ :browser => :chrome, options_key => browser_options })
+  end
+
   @driver = :selenium_chrome_headless
 else
   raise "Navegador Incorreto, variavel @driver está vazia :("
