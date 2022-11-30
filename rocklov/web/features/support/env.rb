8c7adf1e522e0f1d6f5b2e9e0eb47deca3868c87
vitateje <vitateje@gmail.com>
2022-11-30 15:17:05 -0300

    test

--- rocklov/web/features/support/env.rb
+++ rocklov/web/features/support/env.rb
@@ -15,28 +15,26 @@ when "fire_headless"
   @driver = :selenium_headless
 when "chrome_headless"
   Capybara.register_driver :selenium_chrome_headless do |app|
-    version = Capybara::Selenium::Driver.load_selenium
-    options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
+    Capybara::Selenium::Driver.load_selenium
     browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
-      opts.add_argument('--headless')
-      opts.add_argument('--disable-gpu')
-      opts.add_argument('--disable-site-isolation-trials')
-      opts.add_argument('--no-sandbox')
-      opts.add_argument('--disabledev-shm-usage')
+      opts.args << "--headless"
+      opts.args << "--disable-gpu"
+      opts.args << "--disable-site-isolation-trials"
+      opts.args << "--no-sandbox"
+      opts.args << "--disable-dev-shm-usage"
     end
-  
-    Capybara::Selenium::Driver.new(app, **{ :browser => :chrome, options_key => browser_options })
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
   end
 
   @driver = :selenium_chrome_headless
 else
-  raise "Navegador Incorreto, variavel @driver está vazia :("
+  raise "Navegador incorreto, variável @driver está vazia :("
 end
 
 Capybara.configure do |config|
   config.default_driver = @driver
   config.app_host = CONFIG["url"]
-  # Capybara.default_max_wait_time = 10
+  config.default_max_wait_time = 10
 end
 
 AllureCucumber.configure do |config|
